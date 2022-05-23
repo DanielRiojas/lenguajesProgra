036eaa2924ce9239652ab0cc4669c72b558fb190
Mario Garza Chapa <mariogarza@Marios-Air-2.lan>
2022-05-22 22:53:07 -0500

    No edit

--- GUI/src/GUIFrame.java
+++ GUI/src/GUIFrame.java
@@ -146,7 +146,7 @@ public class GUIFrame extends javax.swing.JFrame {
                 {null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "ID Consumidor", "ID Tarea", "Solución", "Contador"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
@@ -159,7 +159,7 @@ public class GUIFrame extends javax.swing.JFrame {
                 {null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "ID Consumidor", "ID Tarea", "Solución", "Contador"
             }
         ));
         jScrollPane2.setViewportView(jTable2);
@@ -244,6 +244,7 @@ public class GUIFrame extends javax.swing.JFrame {
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
+        
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
