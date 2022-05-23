5f6232ee7c8f8e61c913fe08c50ff8148ae4226d
Mario Garza Chapa <mariogarza@Marios-Air-2.lan>
2022-05-22 21:52:35 -0500

    Added flag attribute

--- ProducerConsumer/src/producerconsumer/Scheme.java
+++ ProducerConsumer/src/producerconsumer/Scheme.java
@@ -6,11 +6,13 @@ public class Scheme {
     private char symbol;
     private int num1;
     private int num2;
+    private int flag;
     
     public Scheme(){
         this.symbol = '-';
         this.num1 = 1;
         this.num2 = 2;
+        this.flag = 0;
     }
    
     public void setSymbol(char _symbol){
@@ -24,6 +26,10 @@ public class Scheme {
         num2 = _num2;
     }
 
+    public void setFlag(int _flag){
+        flag = _flag;
+    }
+
     public char getSymbol(){
         return symbol;
     }
@@ -35,6 +41,11 @@ public class Scheme {
     public int getNum2(){
         return num2;
     }
+
+    public int getFlag(){
+        return flag;
+    }
+
 
 }
 
