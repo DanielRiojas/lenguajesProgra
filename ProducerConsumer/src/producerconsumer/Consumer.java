a620701f871a8b3e8601849b9aa682bc59bb302a
Mario Garza Chapa <mariogarza@Marios-Air-2.lan>
2022-05-22 22:49:28 -0500

    Gabo - Added try catch and TODO comments

--- ProducerConsumer/src/producerconsumer/Consumer.java
+++ ProducerConsumer/src/producerconsumer/Consumer.java
@@ -7,16 +7,19 @@ import java.util.logging.Logger;
 
 public class Consumer extends Thread {
     Buffer buffer;
+    int id;
     
-    Consumer(Buffer buffer) {
+    Consumer(Buffer buffer, int id) {
         this.buffer = buffer;
-    }
+        this.id = id;
+    }   
     
     @Override
     public void run() {
         System.out.println("Running Consumer...");
         Scheme product;
 
+        // @Cambiar 5 por tamaño del buffer
         for(int i=0 ; i < 5 ; i++) {
             product = this.buffer.consume();
             
@@ -28,13 +31,21 @@ public class Consumer extends Thread {
                 result = product.getNum1() * product.getNum2();
             }
             else if(product.getSymbol() == '/'){
-                result = product.getNum1() / product.getNum2();
+                try{
+                    result = product.getNum1() / product.getNum2();
+                }catch(ArithmeticException e) {
+                // Exception handler
+                    System.out.println("Divided by zero operation cannot possible");
+                    result = -10000;                
+                }
+                
             }
             else if(product.getSymbol() == '-'){
                 result = product.getNum1() - product.getNum2();
             }
-            System.out.println("Consumer consumed: " + result);
+            System.out.println("Consumer consumed: " + result + " ID: " + id);
             
+            //@TODO cambiar 1000 por timepo de espera de GUI
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException ex) {
